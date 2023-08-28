6ab88407aafe6cb8b484655f242322d6a28bef3c
RubaHoussami <109296044+RubaHoussami@users.noreply.github.com>
2023-08-28 19:49:18 +0300

    Update Write.py
    Signed-off-by: RubaHoussami <109296044+RubaHoussami@users.noreply.github.com>

--- Write.py
+++ Write.py
@@ -6,44 +6,37 @@ table_name = 'Post'
 table = dynamodb.Table(table_name)
 
 def lambda_handler (event, context):
+    
+    print(event)
+    
     try:
-       item_id = event['queryStringParameters']['ID']  # the partition key is the ID - no sort key (maybe we can add one?)
-     
-        # checking if item exists in database
-     
-        response = table.get_item{'ID':obj_id}
-     
-        if 'Item' in response:
-            item = response['Item']
         
-            # dictionary containing requested data
+        response=table.put_item(Item=event)
+        print(response)
         
-            extracted_item = {
-                'ID' : item['ID'],
-                'title' : item.get('title',''),
-                'body' : item.get('body','')
-            }
+        if response['ResponseMetadata']['HTTPStatusCode'] == 200: # checking for success
         
             return {
-                "statusCode" : 200, # OK - success
-                "headers" : {"Content-Type":"application/json"}, # set the HTTP header of the response - JSON data
-                "body" : json.dumps(extracted_item) # turn Python dictionary ito JSON string to be sent as response body
+                "statusCode" : 200,
+                "headers" : {"Content-Type":"application/json"},
+                "body" : json.dumps({"message" : "Item successfully added to dynamodb!"}) 
             }
-      
-        else: # item not found
+            
+        else:
      
             return {
-                "statusCode" : 404, # not found error - failure
-                "headers" : {"Content-Type":"application/json"}, # set the HTTP header of the response - JSON data
-                "body" : json.dumps({"message" : "Item not found!"}) # display message ig
+                "statusCode" : 500,
+                "headers" : {"Content-Type":"application/json"},
+                "body" : json.dumps({"message" : "Failed to add item to dynamodb!"})
                 }
       
     # catch any other errors due to accessing data
     
     except Exception as e:
-     
+        
+        print(e)
         return {
-            "statusCode" : 500, # internal server error - failure
+            "statusCode" : 500,
             "headers" : {"Content-Type":"application/json"},
-            "body" : json.dumps({"message" : "Internal server error!"}) 
+            "body" : json.dumps({"message" : "Internal server error!"})
         }
