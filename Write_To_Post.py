6447c989e87871c7cde18a177cb69c49832b94b0
RubaHoussami <109296044+RubaHoussami@users.noreply.github.com>
2023-09-06 11:33:17 +0300

    Update and rename Write.py to Write_To_Post.py
    Signed-off-by: RubaHoussami <109296044+RubaHoussami@users.noreply.github.com>

--- Write_To_Post.py
+++ Write_To_Post.py
@@ -1,17 +1,31 @@
 import json
 import boto3
+import uuid
 
 dynamodb = boto3.resource('dynamodb')
 table_name = 'Post'
 table = dynamodb.Table(table_name)
 
+
 def lambda_handler (event, context):
     
+    # takes as input username, body, title
     print(event)
     
+    body = json.loads(event['body'])
     try:
         
-        response=table.put_item(Item=event)
+        new_post={
+            'ID': uuid.uuid4().hex,
+            'title' : body['title'],
+            'body' : body['body'],
+            'comments_dictionary' : [],
+            'username' : "user"
+        }
+        
+        print("Make table.put_item call")
+        response=table.put_item(Item=new_post)
+        
         print(response)
         
         if response['ResponseMetadata']['HTTPStatusCode'] == 200: # checking for success
@@ -23,7 +37,6 @@ def lambda_handler (event, context):
             }
             
         else:
-     
             return {
                 "statusCode" : 500,
                 "headers" : {"Content-Type":"application/json"},
@@ -33,7 +46,6 @@ def lambda_handler (event, context):
     # catch any other errors due to accessing data
     
     except Exception as e:
-        
         print(e)
         return {
             "statusCode" : 500,
